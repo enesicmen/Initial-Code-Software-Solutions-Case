4d3894ac79d79731dca6b58ce3d217394371bb54
Enes İçmen <enesicmen66@gmail.com>
2024-07-27 12:44:20 +0300

    Added user picture when registering user

--- app/src/main/java/com/icmen/ecommerceapplication/ui/RegisterActivity.kt
+++ app/src/main/java/com/icmen/ecommerceapplication/ui/RegisterActivity.kt
@@ -1,10 +1,15 @@
 package com.icmen.ecommerceapplication.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.icmen.ecommerceapplication.R
 import com.icmen.ecommerceapplication.databinding.ActivityRegisterBinding
 import com.icmen.ecommerceapplication.ui.common.BaseActivity
@@ -13,6 +18,9 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
+
+    private var selectedImageUri: Uri? = null
 
     override fun setViewBinding(): ActivityRegisterBinding {
         return ActivityRegisterBinding.inflate(layoutInflater)
@@ -21,6 +29,12 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>() {
     override fun initView(savedInstanceState: Bundle?) {
         auth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
+        storage = FirebaseStorage.getInstance()
+
+        getViewBinding()?.profileImageView?.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, REQUEST_CODE_IMAGE_PICK)
+        }
 
         getViewBinding()?.btnRegister?.setOnClickListener {
             val name = getViewBinding()?.etName?.text.toString()
@@ -29,12 +43,21 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>() {
             val email = getViewBinding()?.etEmail?.text.toString()
             val password = getViewBinding()?.etPassword?.text.toString()
 
-
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                registerUser(name, surname, email, city, password)
-            } else {
+            if (name.isEmpty() || surname.isEmpty() || city.isEmpty() || email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, getString(R.string.please_fill_in_all_fields), Toast.LENGTH_SHORT).show()
+            } else if (selectedImageUri == null) {
+                Toast.makeText(this, getString(R.string.please_select_image), Toast.LENGTH_SHORT).show()
+            } else {
+                registerUser(name, surname, email, city, password)
             }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_IMAGE_PICK && resultCode == Activity.RESULT_OK && data != null) {
+            selectedImageUri = data.data
+            getViewBinding()?.profileImageView?.setImageURI(selectedImageUri)
         }
     }
 
@@ -42,34 +65,57 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>() {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-
                     val user: FirebaseUser? = auth.currentUser
                     user?.let {
-                        val userInfo = hashMapOf(
-                            "userId" to it.uid,
-                            "name" to name,
-                            "surname" to surname,
-                            "email" to email,
-                            "city" to city
-                        )
-                        db.collection("users").document(it.uid).set(userInfo)
-                            .addOnSuccessListener {
-                                Toast.makeText(this, getString(R.string.registration_successful), Toast.LENGTH_SHORT).show()
-                                updateUI(user)
-                            }
-                            .addOnFailureListener { e ->
-                                val errorMessage = getString(R.string.adding_to_firestore_failed) +": "+ task.exception?.message
-                                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
-                            }
+                        uploadProfileImage(it.uid, name, surname, email, city)
                     }
                 } else {
-                    val errorMessage = getString(R.string.registration_failed) + ": "+ task.exception?.message
+                    val errorMessage = getString(R.string.registration_failed) + ": " + task.exception?.message
                     Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
                     updateUI(null)
                 }
             }
     }
 
+    private fun uploadProfileImage(userId: String, name: String, surname: String, email: String, city: String) {
+        val storageRef = storage.reference.child("images/profileImages/$userId.jpg")
+        selectedImageUri?.let {
+            storageRef.putFile(it).addOnSuccessListener {
+                storageRef.downloadUrl.addOnSuccessListener { uri ->
+                    saveUserInfo(userId, name, surname, email, city, uri.toString())
+                }
+            }.addOnFailureListener { e ->
+                val errorMessage = getString(R.string.image_upload_failed) + ": " + e.message
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun saveUserInfo(userId: String, name: String, surname: String, email: String, city: String, imageUrl: String?) {
+        val userInfo = hashMapOf(
+            "userId" to userId,
+            "name" to name,
+            "surname" to surname,
+            "email" to email,
+            "city" to city,
+            "profileImageUrl" to imageUrl
+        )
+        db.collection("users").document(userId).set(userInfo)
+            .addOnSuccessListener {
+                Toast.makeText(this, getString(R.string.registration_successful), Toast.LENGTH_SHORT).show()
+                updateUI(auth.currentUser)
+            }
+            .addOnFailureListener { e ->
+                val errorMessage = getString(R.string.adding_to_firestore_failed) + ": " + e.message
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+    }
+
     private fun updateUI(user: FirebaseUser?) {
+        // Kullanıcı güncelleme işlemleri burada yapılabilir
+    }
+
+    companion object {
+        private const val REQUEST_CODE_IMAGE_PICK = 1001
     }
 }
