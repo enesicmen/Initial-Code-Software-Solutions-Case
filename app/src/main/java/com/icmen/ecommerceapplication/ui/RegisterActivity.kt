d6b00948b6adf7893c4cc964817d138abb68e8e2
Enes İçmen <enesicmen66@gmail.com>
2024-07-27 11:57:53 +0300

    User information has been saved to firestore database

--- app/src/main/java/com/icmen/ecommerceapplication/ui/RegisterActivity.kt
+++ app/src/main/java/com/icmen/ecommerceapplication/ui/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import com.icmen.ecommerceapplication.R
 import com.icmen.ecommerceapplication.databinding.ActivityRegisterBinding
 import com.icmen.ecommerceapplication.ui.common.BaseActivity
@@ -11,6 +12,7 @@ import com.icmen.ecommerceapplication.ui.common.BaseActivity
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun setViewBinding(): ActivityRegisterBinding {
         return ActivityRegisterBinding.inflate(layoutInflater)
@@ -18,27 +20,50 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>() {
 
     override fun initView(savedInstanceState: Bundle?) {
         auth = FirebaseAuth.getInstance()
-        getViewBinding()?.registerButton?.setOnClickListener {
-            val email = getViewBinding()?.emailEditText?.text.toString()
-            val password = getViewBinding()?.passwordEditText?.text.toString()
+        db = FirebaseFirestore.getInstance()
+
+        getViewBinding()?.btnRegister?.setOnClickListener {
+            val name = getViewBinding()?.etName?.text.toString()
+            val surname = getViewBinding()?.etSurname?.text.toString()
+            val city = getViewBinding()?.etCity?.text.toString()
+            val email = getViewBinding()?.etEmail?.text.toString()
+            val password = getViewBinding()?.etPassword?.text.toString()
+
 
             if (email.isNotEmpty() && password.isNotEmpty()) {
-                registerUser(email, password)
+                registerUser(name, surname, email, city, password)
             } else {
-                Toast.makeText(this, R.string.please_fill_in_all_fields, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.please_fill_in_all_fields), Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun registerUser(email: String, password: String) {
+    private fun registerUser(name: String, surname: String, email: String, city: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
+
                     val user: FirebaseUser? = auth.currentUser
-                    Toast.makeText(this, R.string.registration_successful, Toast.LENGTH_SHORT).show()
-                    updateUI(user)
+                    user?.let {
+                        val userInfo = hashMapOf(
+                            "userId" to it.uid,
+                            "name" to name,
+                            "surname" to surname,
+                            "email" to email,
+                            "city" to city
+                        )
+                        db.collection("users").document(it.uid).set(userInfo)
+                            .addOnSuccessListener {
+                                Toast.makeText(this, getString(R.string.registration_successful), Toast.LENGTH_SHORT).show()
+                                updateUI(user)
+                            }
+                            .addOnFailureListener { e ->
+                                val errorMessage = getString(R.string.adding_to_firestore_failed) +": "+ task.exception?.message
+                                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+                            }
+                    }
                 } else {
-                    val errorMessage = getString(R.string.registration_failed) + ": " +task.exception?.message
+                    val errorMessage = getString(R.string.registration_failed) + ": "+ task.exception?.message
                     Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
                     updateUI(null)
                 }
